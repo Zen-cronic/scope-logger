d99ac33738264e832f64db4041e72e52a6bb2786
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-03-06 18:14:40 -0500

    update: e2e test

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-command="$1 $2"
-echo "Running command: $command"
-$command
--- tests/e2e/callStackParser.test.process.js
+++ tests/e2e/callStackParser.test.process.js
@@ -3,21 +3,16 @@ const logger = new Logger("Log tester");
 
 (async function () {
   try {
-
-    let testCaseNum = 0
-
     function fn_1() {
       const testArr = [1, 2, 3];
 
       testArr.forEach((number) => {
-          //the log method uses stdout, so this worker's stdout can be piped to main's
+        //the log method uses stdout, so this worker's stdout can be piped to main's
         const { logTitle: result, stack } = logger.log({ number });
 
         // console.error("stack: ", stack);
 
         //forked - own stream
-        // console.error(result);
-        //or
         process.stderr.write(result + "\n");
         // process.stderr.end() //error: end after write
       });
@@ -27,33 +22,49 @@ const logger = new Logger("Log tester");
     }
 
     function fn_2() {
-        const logger = new Logger("Log tester");
-        function inner_fn_2() {
-          const testVari = 123;
-          const { logTitle: result } = logger.log({ testVari });
-  
-          process.stderr.write(result + "\n");
-  
-          // "Log tester: *inner_fn_2* -> *fn_2* -> *Object.<anonymous>*\n");
-        }
-  
-        inner_fn_2();
+      const logger = new Logger("Log tester");
+      function inner_fn_2() {
+        const testVari = 123;
+        const { logTitle: result } = logger.log({ testVari });
+
+        process.stderr.write(result + "\n");
+
+        // "Log tester: *inner_fn_2* -> *fn_2* -> *Object.<anonymous>*\n");
       }
 
+      inner_fn_2();
+    }
+
+    function fn_3() {
+      const logger = new Logger("Log tester");
+
+      const testOuterArr = [1, 2, 3];
+
+      const testInnerArr = [1, 2, 3];
+
+      testOuterArr.map((num) => {
+        testInnerArr.forEach((num) => {
+          const { logTitle: result } = logger.log({ num });
+
+          process.stderr.write(result + "\n");
+        });
+      });
+
+      // process.stderr.write(testOuterArr.length * testInnerArr.length)
+      process.send({ length: testOuterArr.length * testInnerArr.length });
+    }
     //when process.exit()
-    //Log tester: *Array.forEach* -> *fn_1* -> *process.<anonymous>* -> *process.emit* -> *emit* -> *process.processTicksAndRejections*
-    testCaseNum = await new Promise((resolve, reject) => {
+    const testCaseNum = await new Promise((resolve, reject) => {
       process.once("message", (message) => {
         if (typeof message === "number") {
           console.log("message from parent: " + message);
           //   testCaseNumFromParent = message;
 
-          
           // fn_1();
           // process.exit();
-            resolve(message)
+          resolve(message);
         } else {
-          reject(0);
+          reject(new Error("Invalid test case sent from main process"));
         }
       });
     });
@@ -72,23 +83,23 @@ const logger = new Logger("Log tester");
     // });
 
     switch (testCaseNum) {
-        case 1:
-            
-            fn_1()
-            break;
-    
-        case 2:
-            
-            fn_2()
-            break;
-    
-        default:
-            break;
+      case 1:
+        fn_1();
+        break;
+
+      case 2:
+        fn_2();
+        break;
+      case 3:
+        fn_3();
+        break;
+
+      default:
+        break;
     }
 
-    process.exit()
-    // fn_1();
-    // process.exit();
+    //cuz awaited, eth is now sync
+    // process.exit()
   } catch (error) {
     // Send the error message to the parent process
     process.send({ error: error.message });
